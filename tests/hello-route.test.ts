abee9a1876204e27bf54ecefd270f53436ef566d
Mongark <mongarkprogrammer@gmail.com>
2023-04-17 11:40:51 -0400

    First route test

--- tests/hello-route.test.ts
+++ tests/hello-route.test.ts
@@ -1,9 +1,11 @@
-// import app from "../src/app";
-// import supertest from "supertest";
+import app from "../src/app";
+import supertest from "supertest";
 
-// const test_app = supertest(app);
+const test_app = supertest(app);
 
 describe("GET /hello", () => {
-    it("should return a 200 response", () => {
+    it("should return a 200 response", async() => {
+        const res = await test_app.get('/hello');
+        expect(res.statusCode).toEqual(200);
     });
 });
