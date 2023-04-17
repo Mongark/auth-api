d6dd4db0f52e1db30f2f095c58eb386f065eeb92
Mongark <mongarkprogrammer@gmail.com>
2023-04-17 11:16:44 -0400

    Added debugging print on 'hello' route

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -4,6 +4,7 @@ const hello_router = Router();
 
 hello_router.get('/hello', (req: Request, res: Response) => {
     res.status(200).send("hello");
+    console.log("GET /hello");
 });
 
 export default hello_router;
