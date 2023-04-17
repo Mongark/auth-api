a16ed987775b62cd85a631aab7f43b99560c4eb5
Mongark <mongarkprogrammer@gmail.com>
2023-04-17 11:14:29 -0400

    Added first route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import hello_router from "./routes/hello";
 
 const app = express();
+
+app.use(hello_router);
 
 export default app;
---
+++ src/routes/hello.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router } from "express";
+
+const hello_router = Router();
+
+hello_router.get('/hello', (req: Request, res: Response) => {
+    res.status(200).send("hello");
+});
+
+export default hello_router;
