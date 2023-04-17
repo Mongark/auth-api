eab7af7e4d978a01f70a9419f8737fca3705d7fe
Mongark <mongarkprogrammer@gmail.com>
2023-04-17 11:31:05 -0400

    Added testing capabilites

---
+++ tests/hello-route.test.ts
@@ -0,0 +1,9 @@
+// import app from "../src/app";
+// import supertest from "supertest";
+
+// const test_app = supertest(app);
+
+describe("GET /hello", () => {
+    it("should return a 200 response", () => {
+    });
+});
